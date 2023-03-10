3c20aa543a8d5b1e0404bf3816a42589b6e50eff
claimsecond <claim.second@gmail.com>
2023-03-10 17:42:39 +0200

    Finally added the button

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!! I finally get it to work</Text>
+      <View style={styles.header}><Text style={styles.welcome}>Welcome to React Native!</Text></View>
+      <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Button title="Go to Details" />
       <StatusBar style="auto" />
     </View>
   );
